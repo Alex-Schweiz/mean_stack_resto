74c2a5b9d9c40332bc280437f344ee9e87a5be0f
Alex Schweiz <askibinskiy@gmail.com>
2017-02-06 12:53:09 +0200

    Fix. Optimized menu schema

--- app_server/models/menu.js
+++ app_server/models/menu.js
@@ -1,20 +1,14 @@
 var mongoose = require( 'mongoose' );
 
-var oneDishSchema = new mongoose.Schema({
+var menuSchema = new mongoose.Schema({
     name: {type: String, required: true},
     image: {type: String, required: true},
     link: {type: String, required: true},
+    category: {type: String, required: true},
     price: {type: Number, required: true},
     weight: {type: Number, required: true},
     ingredients: {type: String, required: true},
     description: {type: String, required: true}
-});
-
-
-
-var menuSchema = new mongoose.Schema({
-    category: {type: String, required: true},
-    dishes: [oneDishSchema]
 });
 
 mongoose.model('Menu', menuSchema);
